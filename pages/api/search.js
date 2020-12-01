ab00b04f7a8510519b1bb4f0384ccfa42a694527
NgocVo <felix.vo@intelllex.com>
2020-12-01 23:44:02 +0800

    update search query param

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const q = url.searchParams.get("q");
   const resData = await fetch(
-    `https://newsapi.org/v2/everything?q=${q}&apiKey=d0e29e10e21445c5914409cc09352eb4`
+    `https://newsapi.org/v2/everything?q=${q}&pageSize=50&language=en&sortBy=publishedAt&apiKey=d0e29e10e21445c5914409cc09352eb4`
   );
   const data = await resData.json();
 
