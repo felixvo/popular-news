05a7a6753f2d60f4a0d21d1fdd205aba1089112a
NgocVo <felix.vo@intelllex.com>
2020-12-01 23:34:00 +0800

    remove sortBy => default order by publishedAt

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const q = url.searchParams.get("q");
   const resData = await fetch(
-    `https://newsapi.org/v2/everything?q=${q}&sortBy=popularity&apiKey=d0e29e10e21445c5914409cc09352eb4`
+    `https://newsapi.org/v2/everything?q=${q}&apiKey=d0e29e10e21445c5914409cc09352eb4`
   );
   const data = await resData.json();
 
